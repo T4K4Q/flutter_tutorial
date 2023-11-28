fddae7337f838357a510e0d9fdf9656a0e02e721
T4K4Q <kanata.takakyuu.mx@nttdocomo.com>
2023-11-28 14:52:11 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_tutrial
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/flutter_tutrial/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_tutrial
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
